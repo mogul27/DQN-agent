4dad6d00139c066ec49de83a13d64eb97dffebf9
pcoates <pcoates33@gmail.com>
2022-12-12 11:36:54 +0000

    added convnet creation and synch to asynch q_learning

--- pete_folder/asynch_q_learning.py
+++ pete_folder/asynch_q_learning.py
@@ -1,91 +1,354 @@
 import multiprocessing as mp
-import os
-import time
-import numpy as np
 from queue import Empty
+import os
+from pathlib import Path
+import time
+import random
 
-class QFunc:
+import gym
 
-    def __init__(self):
-        self.msg = 'original'
+from dqn_utils import Options
 
-    def display(self):
-        print(f"process id: {os.getpid()} : says {self.msg}")
+# import keras
+from keras.models import Sequential
+from keras import Model
+from keras.layers import Input, Dense, Conv2D, Flatten
+from keras.losses import Huber
+from keras.optimizers import Adam
 
-    def update(self, new_msg):
-        self.msg = new_msg
+import numpy as np
+
+
+class EGreedyPolicy:
+    """ Assumes every state has the same possible actions.
+    """
+
+    def __init__(self, epsilon, q_func, possible_actions, epsilon_decay_span=None, epsilon_min=None):
+        """ e-greedy policy based on the supplied q_table.
+
+        :param epsilon: small epsilon for the e-greedy policy. This is the probability that we'll
+                                    randomly select an action, rather than picking the best.
+        :param q_func: Approximates q values for state action pairs so we can select the best action.
+        :param possible_actions: actions to be selected from with epsilon probability
+        :param epsilon_decay_span: the number of calls over which to decay epsilon
+        :param epsilon_min: the min value epsilon can be after decay
+        """
+        self.q_func = q_func
+        self.epsilon = epsilon
+        self.possible_actions = possible_actions
+        if epsilon_decay_span is None:
+            self.epsilon_min = epsilon
+            self.epsilon_decay = 0
+        else:
+            if epsilon_min is None:
+                self.epsilon_min = 0
+            else:
+                self.epsilon_min = epsilon_min
+            self.epsilon_decay = (self.epsilon - self.epsilon_min) / epsilon_decay_span
+
+    def select_action(self, state):
+        """ The EGreedy policy selects the best action the majority of the time. However, a random action is chosen
+        explore_probability amount of times.
+
+        :param state: The sate to pick an action for
+        :return: selected action
+        """
+        # Select an action for the state, use the best action most of the time.
+        # However, with probability of explore_probability, select an action at random.
+        if np.random.uniform() < self.epsilon:
+            action = self.random_action()
+        else:
+            action = self.q_func.best_action_for(state)
+
+        # decay epsilon for next time
+        if self.epsilon > self.epsilon_min:
+            self.epsilon = max(self.epsilon - self.epsilon_decay, self.epsilon_min)
+
+        return action
+
+    def random_action(self):
+        return random.choice(self.possible_actions)
+
+
+class FunctionApprox:
+
+    def __init__(self, actions, update_batch_size=5, adam_learning_rate=0.0001):
+        self.actions = actions
+        self.q_hat = self.build_cnn(adam_learning_rate)
+        self.q_hat_target = self.build_cnn(adam_learning_rate)
+        self.clone_weights()
+        self.update_batch_size = update_batch_size
+        self.batch = []
+
+    def save_weights(self, file_name):
+        self.q_hat.save_weights(file_name)
+
+    def load_weights(self, file_name):
+        if Path(file_name).exists():
+            self.q_hat.load_weights(file_name)
+            self.q_hat_target.load_weights(file_name)
+
+    def clone_weights(self):
+        # Copy the weights from action_value network to the target action_value network
+        self.q_hat_target.set_weights(self.q_hat.get_weights())
+
+    def get_value_network_weights(self):
+        return self.q_hat.get_weights()
+
+    def set_value_network_weights(self, weights):
+        self.q_hat.set_weights(weights)
+
+    def get_target_network_weights(self):
+        self.q_hat_target.get_weights()
+
+    def set_target_network_weights(self, weights):
+        self.q_hat_target.set_weights(weights)
+
+    def build_cnn(self, adam_learning_rate):
+        # Crete CNN model to predict actions for states.
+
+        try:
+            # TODO : give all the layers and models names to indicate worker / controller ?
+            # inputs = Input((84, 84, 4))
+            # conv_1 = Conv2D(32, kernel_size=(8, 8), strides=(4, 4), activation='relu')(inputs)
+            # conv_2 = Conv2D(64, kernel_size=(4, 4), strides=(2, 2), activation='relu')(conv_1)
+            # conv_3 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation='relu')(conv_2)
+            # flatten = Flatten()(conv_3)
+            # dense_1 = Dense(512, activation='relu')(flatten)
+            # outputs = Dense(4, activation=None)(dense_1)
+            # cnn = Model(inputs=inputs, outputs=outputs)
+            cnn = Sequential()
+            # TODO : The async paper uses fewer and smaller.
+            cnn.add(Conv2D(32, kernel_size=(8, 8), strides=(4, 4), activation='relu', input_shape=(84, 84, 4)))
+            cnn.add(Conv2D(64, kernel_size=(4, 4), strides=(2, 2), activation='relu'))
+            cnn.add(Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
+            cnn.add(Flatten())
+            cnn.add(Dense(512, activation='relu'))
+            cnn.add(Dense(4, activation=None))
+            cnn.summary()
+
+            # compile the model
+            cnn.compile(loss=Huber(delta=1.0), optimizer=Adam(learning_rate=adam_learning_rate))
+
+            return cnn
+        except Exception as e:
+            print(f"failed to create model : {e}")
+
+    def transpose_states(self, states):
+        # states is a 4D array (N, X,Y,Z) with
+        # N = number of states,
+        # X = state and history, for CNN we need to transpose it to (N, Y,Z,X)
+        # and also add another level.
+        return np.transpose(np.array(states), (0, 2, 3, 1))
+
+    def get_value(self, state, action):
+        prediction = self.q_hat.predict_on_batch(self.transpose_states([state]))
+        return prediction[0][action]
+
+    def get_all_action_values(self, states):
+        return self.q_hat.predict_on_batch(self.transpose_states(states))
+
+    def get_target_value(self, state, action):
+        prediction = self.q_hat_target.predict_on_batch(self.transpose_states([state]))
+        return prediction[0][action]
+
+    def get_max_target_value(self, state):
+        prediction = self.q_hat_target.predict_on_batch(self.transpose_states([state]))
+        return max(prediction[0])
+
+    def get_max_target_values(self, states):
+        predictions = self.q_hat_target.predict_on_batch(self.transpose_states(states))
+        return predictions.max(axis=1)
+
+    def best_action_for(self, state):
+        prediction = self.q_hat.predict_on_batch(self.transpose_states([state]))
+        return np.argmax(prediction[0])
+
+    def update(self, state, new_action_values):
+        # do the update in batches
+        self.batch.append((state, new_action_values))
+        if len(self.batch) < self.update_batch_size:
+            return
+
+        states = np.array([s for (s, new_action_value) in self.batch])
+        states = self.transpose_states(states)
+        new_action_values = np.array([new_action_value for (s, new_action_value) in self.batch])
+
+        self.q_hat.train_on_batch(states, new_action_values)
+
+        # clear the batch.
+        self.batch = []
 
 
 class AsyncQLearnerWorker(mp.Process):
 
-    def __init__(self, messages, network_gradient_queue):
+    def __init__(self, messages, network_gradient_queue, options=None):
+        super().__init__()
+        # messages is a mp.Queue used to receive messages from the controller
         self.messages = messages
         self.network_gradient_queue = network_gradient_queue
-        super().__init__()
+        self.options = Options(options)
+
+        self.options.default('async_update_freq', 5)
+        self.options.default('env_name', "ALE/Breakout-v5")
+        self.options.default('actions', [0, 1, 2, 3])
+
+        # keep track of requests from the controller by recording them in the tasks dict.
+        self.tasks = {
+            'value_network_weights': None,
+            'target_network_weights': None,
+            'reset_network_weights': None
+        }
+
+        self.q_func = FunctionApprox(self.options.get('actions'))
+
+        # TODO : add epsilon to options
+        self.policy = EGreedyPolicy(1.0, self.q_func, self.options.get('actions'),
+                                    epsilon_decay_span=1000, epsilon_min=0.1)
+
+        self.gradient_deltas = None
+        self.initialised = False
+
+    def get_latest_tasks(self):
+        """ Get the latest tasks from the controller.
+
+        Overrides any existing task of the same name.
+        """
+        read_messages = True
+        while read_messages:
+            try:
+                msg_code, content = self.messages.get(False)
+                print(f"worker {self.pid}: got message {msg_code}")
+
+                if msg_code in self.tasks:
+                    self.tasks[msg_code] = content
+                else:
+                    print(f"worker {self.pid}: ignored unrecognised message {msg_code}")
+            except Empty:
+                # Nothing to process, so just carry on
+                read_messages = False
+
+    def process_controller_messages(self):
+        """ see if there are any messages from the controller, and process accordingly
+        """
+        self.get_latest_tasks()
+        if self.tasks['value_network_weights'] is not None:
+            print(f"worker {self.pid}: update the value network weights")
+            self.q_func.set_value_network_weights(self.tasks['value_network_weights'])
+            self.tasks['value_network_weights'] = None
+
+        if self.tasks['target_network_weights'] is not None:
+            print(f"worker {self.pid}: update the target network weights")
+            self.q_func.set_target_network_weights(self.tasks['target_network_weights'])
+            self.tasks['target_network_weights'] = None
+
+        if self.tasks['reset_network_weights'] is not None:
+            print(f"worker {self.pid}: reset both value and target network weights")
+            network_weights = self.tasks['reset_network_weights']
+            self.q_func.set_value_network_weights(network_weights)
+            self.q_func.set_target_network_weights(network_weights)
+
+            self.gradient_deltas = network_weights
+            for layer_weights in network_weights:
+                layer_weights.fill(0.1)
+            self.initialised = True
+            self.tasks['reset_network_weights'] = None
 
     def run(self):
         print(f"I am a worker, my PID is {self.pid}")
+        steps = 0
+        steps_since_async_update = 0
+
+        env = gym.make(self.options.get('env_name'), obs_type="grayscale", frameskip=1)
+        episode = 1
 
         while True:
-            # see if there are any messages
-            if self.messages.poll():
-                msg = self.messages.recv()
-                print(f"worker {self.pid}: got message {msg}")
 
-                self.network_gradient_queue.put(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+            self.process_controller_messages()
+
+            if steps_since_async_update >= self.options.get('async_update_freq'):
+                self.network_gradient_queue.put(
+                    (
+                        steps_since_async_update,
+                        self.gradient_deltas
+                     )
+                )
+                steps_since_async_update = 0
+
+            if self.initialised:
+                # take a step and calculate the gradient deltas.
+                steps += 1
+                steps_since_async_update += 1
 
 
 class AsyncQLearningController:
 
-    def __init__(self, num_workers=1):
+    def __init__(self, actions, num_workers=1, options=None):
+        self.actions = actions
         self.num_workers = num_workers
+        self.options = Options(options)
+        self.q_func = FunctionApprox(actions, options)
+        self.workers = []
+
+    def message_all_workers(self, msg_code, content):
+        msg = (msg_code, content)
+        for worker, worker_queue in self.workers:
+            # send message to each worker
+            print(f"controller sending : {msg_code}")
+            worker_queue.put(msg)
+            print(f"controller sent : {msg_code}")
+
+    def update_value_network_weights(self, gradient_deltas):
+        value_network_weights = self.q_func.get_value_network_weights()
+        for (weights, deltas) in zip(value_network_weights, gradient_deltas):
+            weights += deltas
+
+        self.q_func.set_value_network_weights(value_network_weights)
 
     def train(self):
         print('parent process:', os.getppid())
+        # Need to make sure workers are spawned rather than forked otherwise keras gets into a deadlock. Which seems
+        # to be due to multiple processes trying to share the same default tensorflow graph.
+        mp.set_start_method('spawn')
 
-        network_gradient_queue = mp.Queue(100)
+        # TODO : does this need a max size setting?
+        network_gradients_queue = mp.Queue(100)
 
         # set up the workers
-        # TODO : should we make it spawn?
-        workers = []
+
         for _ in range(self.num_workers):
-            messages, child_conn = mp.Pipe()
-            worker = AsyncQLearnerWorker(child_conn, network_gradient_queue)
+            worker_queue = mp.Queue(100)
+            worker = AsyncQLearnerWorker(worker_queue, network_gradients_queue, self.options)
             worker.daemon = True    # helps tidy up child processes if parent dies.
             worker.start()
-            workers.append((worker, messages))
+            self.workers.append((worker, worker_queue))
 
-        data = ['one', 'two', 'three', np.array([[1], [2], [3]])]
-        data_item = 0
-        gradients_recieved = 0
+        # Send the workers the starting weights
+        self.message_all_workers('reset_network_weights', self.q_func.get_value_network_weights())
 
-        while data_item < len(data) or gradients_recieved < 4:
-            if data_item < len(data):
-                item = data[data_item]
-                data_item += 1
+        total_steps = 0
 
-                for worker, messages in workers:
-                    # send message to each worker
-                    print(f"controller sent item {item}")
-                    messages.send(item)
+        while total_steps < 10:
 
-            if gradients_recieved < 4:
-                try:
-                    network_gradient_deltas = network_gradient_queue.get(False)
-                    print(f"controller got network gradients: {network_gradient_deltas}")
-                    gradients_recieved += 1
-                except Empty:
-                    # Nothing to process, so just carry on
-                    pass
+            try:
+                (steps, gradient_deltas) = network_gradients_queue.get(False)
+                print(f"controller got network gradients")
+                total_steps += steps
+                # update the value network weights.
+                self.update_value_network_weights(gradient_deltas)
+
+            except Empty:
+                # Nothing to process, so just carry on
+                pass
 
         # close down the workers
-        for worker, messages in workers:
+        for worker, _ in self.workers:
             worker.terminate()
             worker.join(1)
 
 
 def create_and_run_agent():
-    agent = AsyncQLearningController()
+    agent = AsyncQLearningController([0, 1, 2, 3])
     agent.train()
 
 
